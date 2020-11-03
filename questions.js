a0a241899a884d0a8b7afa1b944ae952dad3df06
rswalm <62914913+rswalm@users.noreply.github.com>
2020-11-02 23:01:26 -0500

    all quize code

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "Whose death sparked World War I?",
+        choices: ["Kaiser Wilhelm", "Archbishop Ussher", "Queen Victoria", "Archduke Franz Ferdinand"],
+        answer: "Archduke Franz Ferdinand"
+    },
+
+    {
+        title: "Which of these nations was neutral in World War I?",
+        choices: ["Germany", "Norway", "Italy", "England"],
+        answer: "Norway"
+    },
+
+    {
+        title: "Which of these ships was sunk by a German submarine?",
+        choices: ["Arizona", "Lusitania", "Titanic", "Andrea Doria"],
+        answer: "Lusitania"
+    },
+
+    {
+        title: "World War I ended in:",
+        choices: ["1925", "1918", "1920", "1915"],
+        answer: "1918"
+    },
+
+    {
+        title: "Which of these people was a spy in World War I?",
+        choices: ["James Bond", "Mata Hari", "Benedict Arnold", "Serge Plekhanov"],
+        answer: "Mata Hari"
+    },
+
+    {
+        title: "Who was the first U.S. president to appear on television?",
+        choices: ["Richard Nixon", "Ronald Reagan", "Abraham Lincoln", "Franklin Delano Roosevelt"],
+        answer: "Franklin Delano Roosevelt"
+    },
+
+
+];
